766c55ad810ff9384db3a1c9051236bacb336090
Miyako-49172 <38390594+Miyako-49172@users.noreply.github.com>
2018-04-21 02:53:18 +0000

    Update Sub_Pi.py
    Corrected pump activation timer

--- Sub_Pi.py
+++ Sub_Pi.py
@@ -2,10 +2,11 @@ import bluetooth
 import RPi.GPIO as GPIO
 #Server Adresse ['B8:27:EB:7F:76:B1']
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3,GPIO.OUT) # Correct with output for pump transistor
-GPIO.setup(15,GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(17,GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(18,GPIO.IN,pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(3,GPIO.OUT) # Pump transistor
+GPIO.setup(14,GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #Mint
+GPIO.setup(15,GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #Strtawberry
+GPIO.setup(17,GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #Rose
+GPIO.setup(18,GPIO.IN,pull_up_down = GPIO.PUD_DOWN) #Piment
 
 
 
@@ -18,22 +19,28 @@ def Soil_Hydrometry(n):
         _Soil_Level = GPIO.input(17) #Input correct soil sensor
     if n ==3:
         _Soil_Level = GPIO.input(18) #Input correct soil sensor
+    if n ==4:
+        _Soil_Level = GPIO.input(14) #Input correct soil sensor
         
     ### Add other sensor if necessary
     return _Soil_Level
 
 def Activate_pump(n):
-    """Activate the corresponding pump"""
+    """Activate the corresponding pump with appropriate valve
+       Not available yet so depending of the distance of the plants pump longer"""
     #Need additionnal code for valve control
     if n==1:
         GPIO.output(3,1) # Correct with the pin of the transistor
-        time.sleep(15)
+        time.sleep(5)
     if n==2:
         GPIO.output(3,1) # Correct with the pin of the transistor
-        time.sleep(15)
-     if n==3:
+        time.sleep(5)
+    if n==3:
         GPIO.output(3,1) # Correct with the pin of the transistor
-        time.sleep(15)
+        time.sleep(10)
+    if n==4:
+        GPIO.output(3,1) # Correct with the pin of the transistor
+        time.sleep(10)
         
 #Main Loop for client
 server_socket=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -52,15 +59,20 @@ while True:
         data_to_send = str(Soil_Hydrometry(2))
     elif "SOIL3" in data_to_receive:
         data_to_send = str(Soil_Hydrometry(3))
+    elif "SOIL4" in data_to_receive:
+        data_to_send = str(Soil_Hydrometry(4))
     elif "PUMP1" in data_to_receive:
         Activte_pump(1)
-        data_to_send = "Watering"
+        data_to_send = "Watering_Strawberry"
     elif "PUMP2" in data_to_receive:
         Activte_pump(2)
-        data_to_send = "Watering"
+        data_to_send = "Watering_Rose"
     elif "PUMP3" in data_to_receive:
         Activte_pump(3)
-        data_to_send = "Watering"
+        data_to_send = "Watering_Piment"
+    elif "PUMP4" in data_to_receive:
+        Activte_pump(4)
+        data_to_send = "Watering_Mint"
 
 
     client_socket.send(data_to_send)
